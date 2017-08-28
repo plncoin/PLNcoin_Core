43ba2b5852b6fb7768a8764b93b0d7233724bc33
Rav3nPL <rav3n.pl@gmail.com>
2017-08-28 02:23:51 +0200

    Rebranding cd

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("PLNc");
+    case mBTC: return QString("mPLNc");
+    case uBTC: return QString::fromUtf8("μPLNc");
     default: return QString("???");
     }
 }
